32122f6e23cb89588e24bbb5ee615cb4eba51405
Matias Jaubet <matias.jaubet@dhnn.com>
2026-02-03 10:52:52 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Menu, X } from 'lucide-react';
 
 const LINKS = [
     { name: 'Inicio', href: '/' },
-    { name: 'Hostería', href: '/hosteria' },
+    { name: 'Hosteríaa', href: '/hosteria' },
     { name: 'Habitaciones', href: '/habitaciones' },
     { name: 'Servicios', href: '/servicios' },
     { name: 'Galería', href: '/galeria' },
